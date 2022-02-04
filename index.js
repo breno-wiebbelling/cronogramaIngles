660d3f6e1b16fb803c544dbbf60e229d88e7a1b9
Breno Wiebbelling <brenowdea@gmail.com>
2022-02-04 10:42:14 -0300

    sd

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function enviar(){
     function coletando(){
         var la, ua, natual, na, resultado;
 
-
-        /*
         la = document.getElementById('la').value
         ua = document.getElementById('ua').value
         natual = document.getElementById('natual').value
@@ -17,77 +15,46 @@ function enviar(){
         natual = parseInt(natual)
         na = parseInt(na)
 
-        var nr = 16-natual;
-        var nr = nr+na;
-        
-        */
-
-        la = 2
-        ua = 3
-        natual = 13
-    
-        na = 0 
-        ld = 4
-        resultado = ''
-
         nr = 16-natual;
         nr = nr+na;
-
-        //dados recolhidos
-        /*
-        console.log(`${la} -- la`)K
-        console.log(`${ua} -- ua`)
-        console.log(`${natual} -- natual`)
-        console.log(`${na} -- na`)
-        console.log(`${ld} -- ld`)
-        console.log(`${nr} -- nr`)
-        console.log('')
-        */
-
+        
 
         //parte 1
         var lr = 4-la 
-        //console.log(`${lr} -- Lições restantes na unidade`)
 
 
         //parte 2
         var ur = 6-ua
         var lu = ur*4
         var lt = lr+lu
-        //console.log(`${lt} -- Lições totais no nível`)
         
 
         //parte 3
         var lr = nr*4
         var lr = lr*6
-        //console.log(`${lr} -- Lições dos outros níveis`)
 
 
         //parte 4
         lt = lt+lr
-        //console.log(`${lt} -- Lições totais`)
         
-        //window.alert(lt)
-        console.log(`Lt --> ${lt}`)
 
         return lt
     }
     
     function data(){
     
-    data = new Date()
+        data = new Date()
     
-    
-    dia = parseInt(data.getDate())
-    mes = parseInt(data.getMonth())+1
+        dia = parseInt(data.getDate())
+        mes = parseInt(data.getMonth())+1
 
-    //dias restantes no mês
-    var drm = 30-dia
+        //dias restantes no mês
+        var drm = 30-dia
 
-    
+        
 
-    var dias = [drm, mes, dia]
-    return dias
+        var dias = [drm, mes, dia]
+        return dias
     }
     
 
@@ -117,9 +84,8 @@ function enviar(){
     }
 
     mensagem = (`Dia: ${densidade.toFixed(1)} \nMês: ${mes}`)
-    //resultado.innerHTML = (mensagem)
+    resultado.innerHTML = (mensagem)
     console.log(mensagem)
 }
-
 
 enviar()
